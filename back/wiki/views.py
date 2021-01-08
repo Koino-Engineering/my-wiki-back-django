d379a529213703c6e3f04a2b92d4e69b1fa5009f
KoinoEngineering <email@koino.engineering>
2021-01-08 13:04:35 +0900

    #1 add TokenAuthentication to Article

--- back/wiki/views.py
+++ back/wiki/views.py
@@ -1,5 +1,5 @@
 from rest_framework import permissions, viewsets
-from rest_framework.authentication import SessionAuthentication
+from rest_framework.authentication import SessionAuthentication, TokenAuthentication
 
 from wiki.models import Article
 from wiki.serializers import ArticleSerializer
@@ -11,7 +11,7 @@ class ArticleViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows users to be viewed or edited.
     """
-    authentication_classes = [SessionAuthentication]
+    authentication_classes = [SessionAuthentication, TokenAuthentication]
     queryset = Article.objects.all().order_by('-created_at')
     serializer_class = ArticleSerializer
     permission_classes = [permissions.IsAuthenticated]
